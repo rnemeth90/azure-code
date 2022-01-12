194380a15ee0ce02570506a250223dbe465d533b
Ryan Nemeth <Ryan.Nemeth@aprimo.com>
2022-01-12 11:38:07 -0500

    adding a gitignore file and ignoring some files

---
+++ AzureSqlDbApp/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AzureSqlDbApp.Models;
+using AzureSqlDbApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureSqlDbApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public CourseController(IConfiguration _config, CourseService _svc)
+        {
+            _course_service = _svc;
+            _configuration = _config;
+        }
+
+        // The Index method is used to get a list of courses and return it to the view
+        public IActionResult Index()
+        {
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
+
---
+++ AzureSqlDbApp/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureSqlDbApp.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
