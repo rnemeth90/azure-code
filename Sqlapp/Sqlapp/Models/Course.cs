9f71786e7fb4bbff7f52b6a2a43ccf0b24a8fa8e
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-03-15 10:24:20 -0400

    updates

--- Sqlapp/Sqlapp/Models/Course.cs
+++ Sqlapp/Sqlapp/Models/Course.cs
@@ -7,9 +7,8 @@ namespace Sqlapp.Models
 {
     public class Course
     {
-        // This class represents the struture of our data
+        // This class represents the structure of our data
         public int CourseID { get; set; }
-
         public string CourseName { get; set; }
         public decimal Rating { get; set; }
     }
